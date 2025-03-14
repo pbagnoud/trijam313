4270828e697b6c6e15e80753061caaf86ec7903c
PB <119977776+pbagnoud@users.noreply.github.com>
2025-03-14 21:23:08 +0100

    Revert "e"
    This reverts commit da43512096261b8529d2db78226d82e24fd7b578.

---
+++ trijam-313/script/food.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+var speed = 300
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@export var food_id = 0
+
+func _ready() -> void:
+	food_id = randi() % 3
+	animated_sprite_2d.frame = food_id
+func _process(delta):
+	position.x += speed*delta
+
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	print(event)
+	if event is InputEventMouseButton and event.pressed:
+		queue_free()
