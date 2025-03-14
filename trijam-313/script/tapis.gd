b74a6bec11db716b5947bafd6f193dec8313ebea
SolennEstier <solenn.estier@gmail.com>
2025-03-14 21:38:54 +0100

    changed font

--- trijam-313/script/tapis.gd
+++ trijam-313/script/tapis.gd
@@ -12,4 +12,4 @@ func _on_score_up_difficulty() -> void:
 	var my_random_number = rng.randf_range(0.3, 0.5)
 	$food_spawn.wait_time -= my_random_number
 	print("Ouhla c'est plus dur")
-	pass # Replace with function body.
+	
