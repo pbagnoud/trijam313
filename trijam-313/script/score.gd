c8b9f6b7527a93032cc7ebd7e006ef4fe797edaa
SolennEstier <solenn.estier@gmail.com>
2025-03-14 21:03:49 +0100

    Added quickening

--- trijam-313/script/score.gd
+++ trijam-313/script/score.gd
@@ -1,12 +1,21 @@
 extends Control
 
+signal up_difficulty
+
+var limit1 : int = 15
+var limit2 : int = 30
+
 @onready var score_text: RichTextLabel = $Score_text
 
 @onready var value : int = 0
 
+
+
 func increment() :
 	value += 1
 	score_text.text = "Score : " + str(value)
+	if value == limit1 or value == limit2 :
+		up_difficulty.emit()
 	
 	
 
