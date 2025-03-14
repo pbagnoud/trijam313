abcc2ec2780a99e9f5d832b9790d904033b7a569
PB <119977776+pbagnoud@users.noreply.github.com>
2025-03-14 20:07:15 +0100

    food update

--- trijam-313/scene/food.gd
+++ trijam-313/scene/food.gd
@@ -3,3 +3,9 @@ extends CharacterBody2D
 var speed = 300
 func _process(delta):
 	position.x += speed*delta
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	print(event)
+	if event is InputEventMouseButton and event.pressed:
+		queue_free()
