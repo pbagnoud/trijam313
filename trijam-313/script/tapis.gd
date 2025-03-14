8647045648ed2079020b2119631b7e46491a0512
SolennEstier <solenn.estier@gmail.com>
2025-03-14 21:47:46 +0100

    changed spawn

--- trijam-313/script/tapis.gd
+++ trijam-313/script/tapis.gd
@@ -1,7 +1,16 @@
 extends Node2D
 const FOOD = preload("res://scene/food.tscn")
 
+@onready var start_timer: Timer = $Start_timer
+
+
 var rng = RandomNumberGenerator.new()
+var start_random = rng.randf_range(0, 2)
+
+func _ready():
+	start_timer.wait_time = start_random
+	start_timer.start()
+
 
 func _on_food_spawn_timeout() -> void:
 	var instance = FOOD.instantiate()
@@ -13,3 +22,8 @@ func _on_score_up_difficulty() -> void:
 	$food_spawn.wait_time -= my_random_number
 	print("Ouhla c'est plus dur")
 	
+
+
+func _on_start_timer_timeout() -> void:
+	print("fini")
+	$food_spawn.start()
