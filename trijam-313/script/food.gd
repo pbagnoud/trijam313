9ca56269cb24d0abd72785fa92a4e26383a1d5b5
charl-baum <charlottebaumgart61@gmail.com>
2025-03-14 22:49:31 +0100

    Son qui marche pas

--- trijam-313/script/food.gd
+++ trijam-313/script/food.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 var speed = 300
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @export var food_id = 0
+@onready var hit_sound: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
 
 func _ready() -> void:
 	food_id = randi() % 3
@@ -12,8 +14,8 @@ func _process(delta):
 	position.x += speed*delta
 
 
-
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	print(event)
 	if event is InputEventMouseButton and event.pressed:
+		hit_sound.play()
 		queue_free()
